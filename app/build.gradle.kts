1c218403bfd456918636efb01c1f72d440e2288b
Magda <266857@student.pwr.edu.pl>
2024-01-07 16:03:37 +0100

    add camera feature

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("com.google.android.gms:play-services-mlkit-text-recognition-common:19.0.0")
     implementation("com.google.android.gms:play-services-mlkit-text-recognition:19.0.0")
+    implementation("androidx.camera:camera-view:1.3.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -69,4 +70,21 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
+    implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.6.2")
+
+    implementation("androidx.camera:camera-camera2:1.1.0")
+//    implementation("androidx.camera.lifecycle:lifecycle-camera:1.1.0")
+    implementation("androidx.camera:camera-view:1.1.0")
+    implementation("androidx.camera:camera-extensions:1.1.0")
+
+    //// ACCOMPANIST ////
+    implementation("com.google.accompanist:accompanist-permissions:0.32.0")
+
+    implementation("com.google.mlkit:image-labeling:17.0.5")
+//    implementation("com.google.mlkit:text-recognition:17.0.2")
 }
--- app/src/main/java/com/example/smartcamera/screens/CameraScreen.kt
+++ app/src/main/java/com/example/smartcamera/screens/CameraScreen.kt
@@ -1,43 +1,161 @@
 package com.example.smartcamera.screens
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.util.Log
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.widget.LinearLayout
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.ImageProxy
+import androidx.camera.view.LifecycleCameraController
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment.Companion.BottomStart
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.smartcamera.camera.CameraState
+import com.example.smartcamera.camera.CameraViewModel
+import java.util.concurrent.Executor
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CameraScreen() {
-    var text by remember { mutableStateOf("") }
+    val viewModel: CameraViewModel = viewModel()
+    val cameraState: CameraState by viewModel.state.collectAsStateWithLifecycle()
 
-    Scaffold(
-        topBar = {
-            TopAppBar(title = { Text("Rozpoznawanie Obiektów i Tekstu") })
-        },
-        content = {
-            Column {
-                Button(onClick = {
-                    // TODO: Rozpocznij proces robienia zdjęcia
-                }) {
-                    Text("Zrób Zdjęcie")
-                }
-
-                Text(text) // Wyświetla wyniki rozpoznawania
-            }
-        }
+    CameraContent(
+        onPhotoCaptured = viewModel::storePhotoInGallery,
+        lastCapturedPhoto = cameraState.capturedImage
     )
 }
 
-@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DefaultPreview() {
-    CameraScreen()
+private fun CameraContent(
+    onPhotoCaptured: (Bitmap) -> Unit,
+    lastCapturedPhoto: Bitmap? = null
+) {
+
+    val context: Context = LocalContext.current
+    val lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current
+    val cameraController: LifecycleCameraController = remember { LifecycleCameraController(context) }
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                text = { Text(text = "Take photo") },
+                onClick = { capturePhoto(context, cameraController, onPhotoCaptured) },
+                icon = {  }
+            )
+        }
+    ) { paddingValues: PaddingValues ->
+
+        Box(modifier = Modifier.fillMaxSize()) {
+            AndroidView(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues),
+                factory = { context ->
+                    PreviewView(context).apply {
+                        layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
+                        setBackgroundColor(Color.BLACK)
+                        implementationMode = PreviewView.ImplementationMode.COMPATIBLE
+                        scaleType = PreviewView.ScaleType.FILL_START
+                    }.also { previewView ->
+                        previewView.controller = cameraController
+                        cameraController.bindToLifecycle(lifecycleOwner)
+                    }
+                }
+            )
+
+            if (lastCapturedPhoto != null) {
+                LastPhotoPreview(
+                    modifier = Modifier.align(alignment = BottomStart),
+                    lastCapturedPhoto = lastCapturedPhoto
+                )
+            }
+        }
+    }
+}
+
+private fun capturePhoto(
+    context: Context,
+    cameraController: LifecycleCameraController,
+    onPhotoCaptured: (Bitmap) -> Unit
+) {
+    val mainExecutor: Executor = ContextCompat.getMainExecutor(context)
+
+    cameraController.takePicture(mainExecutor, object : ImageCapture.OnImageCapturedCallback() {
+        override fun onCaptureSuccess(image: ImageProxy) {
+            val correctedBitmap: Bitmap = image
+                .toBitmap()
+
+            onPhotoCaptured(correctedBitmap)
+            image.close()
+        }
+
+        override fun onError(exception: ImageCaptureException) {
+            Log.e("CameraContent", "Error capturing image", exception)
+        }
+    })
+}
+
+@Composable
+private fun LastPhotoPreview(
+    modifier: Modifier = Modifier,
+    lastCapturedPhoto: Bitmap
+) {
+
+    val capturedPhoto: ImageBitmap = remember(lastCapturedPhoto.hashCode()) { lastCapturedPhoto.asImageBitmap() }
+
+    Card(
+        modifier = modifier
+            .size(128.dp)
+            .padding(16.dp),
+        shape = MaterialTheme.shapes.large
+    ) {
+        Image(
+            bitmap = capturedPhoto,
+            contentDescription = "Last captured photo",
+            contentScale = androidx.compose.ui.layout.ContentScale.Crop
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun Preview_CameraContent() {
+    CameraContent(
+        onPhotoCaptured = {}
+    )
 }
